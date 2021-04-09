47ab68b189ba16244fc8e0b287ecec9568343f99
Stone98 <stone18003@gamil.com>
2021-04-09 11:54:38 -0700

    server comments

--- api/server.js
+++ api/server.js
@@ -6,14 +6,16 @@ const projectsRouter = require("./projects/projects-router");
 const server = express();
 
 // global middlewares
-server.use(express.json());
-server.use(cors());
-server.use(helmet());
+server.use(express.json()); // parse requests with JSON
+server.use(cors()); // restricts server from being accessed by other sites or domains
+server.use(helmet()); // protects the app by setting HTTP headers
 
+// enables routes for actions and projects
 server.use("/api/actions", actionsRouter);
 server.use("/api/projects", projectsRouter);
 
-server.use("/", (req, res) => {
+// catch all endpoint that indicates that server/api is up
+server.use("*", (req, res) => {
   res.send(`<h1>API is up!</h1>`);
 });
 
