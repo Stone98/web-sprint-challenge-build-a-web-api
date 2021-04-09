371ed9526256b41e42a188d728a0c31212d93129
Stone98 <stone18003@gamil.com>
2021-04-09 11:48:25 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const actionsRouter = require("./actions/actions-router");
 const projectsRouter = require("./projects/projects-router");
 const server = express();
 
+// global middlewares
 server.use(express.json());
 server.use(cors());
 server.use(helmet());
@@ -12,7 +13,7 @@ server.use(helmet());
 server.use("/api/actions", actionsRouter);
 server.use("/api/projects", projectsRouter);
 
-server.use("*", (req, res) => {
+server.use("/", (req, res) => {
   res.send(`<h1>API is up!</h1>`);
 });
 
