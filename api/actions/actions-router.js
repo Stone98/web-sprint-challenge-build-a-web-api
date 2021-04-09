9383d86b26cac330a1a257a3db955c421b571a6a
Stone98 <stone18003@gamil.com>
2021-04-09 10:24:49 -0700

    wip

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,25 @@
-// Write your "actions" router here!
+const router = require("express").Router();
+const Action = require("./actions-model");
+
+router.get("/", (req, res, next) => {
+  Action.get()
+    .then((actions) => {
+      res.json(actions);
+    })
+    .catch(next);
+});
+
+router.get("/:id", (req, res) => {
+  res.json(req.action);
+});
+
+// eslint-disable-next-line no-unused-vars
+router.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+    custom: "something went wrong in the actions router",
+  });
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 const actionsRouter = require("./actions/actions-router");
 const projectsRouter = require("./projects/projects-router");
 const server = express();
@@ -6,6 +8,8 @@ const server = express();
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
 server.use(express.json());
+server.use(cors());
+server.use(helmet());
 
 server.use("/api/actions", actionsRouter);
 server.use("/api/projects", projectsRouter);
