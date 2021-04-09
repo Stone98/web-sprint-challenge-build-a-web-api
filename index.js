617c7ddc8baa0952d2969f2842696c55a45e959a
Stone98 <stone18003@gamil.com>
2021-04-09 12:21:59 -0700

    mvp

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const dotenv = require("dotenv").config(); // hooks up to .env file to get enviroment variables
+require("dotenv").config(); // hooks up to .env file to get enviroment variables
 const server = require("./api/server"); // hooks up the server to index.js so it can run with either node or nodemon
 
 const PORT = process.env.PORT || 4000; // uses PORT variable in .env or defaults to 4000
