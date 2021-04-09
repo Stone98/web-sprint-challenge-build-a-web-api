7a1a2bc12e62cd01afed2c28d67e033cc32b1e45
Stone98 <stone18003@gamil.com>
2021-04-09 10:50:14 -0700

    tests passing

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -10,6 +10,47 @@ router.get("/", (req, res, next) => {
     .catch(next);
 });
 
+router.get("/:id", mw.validateProjectId, (req, res) => {
+  res.json(req.project);
+});
+
+router.post("/", mw.validateProject, (req, res, next) => {
+  Project.insert(req.body)
+    .then((project) => {
+      res.status(201).json(project);
+    })
+    .catch(next);
+});
+
+router.put(
+  "/:id",
+  mw.validateProjectId,
+  mw.validateProject,
+  (req, res, next) => {
+    Project.update(req.params.id, req.body)
+      .then((project) => {
+        res.json(project);
+      })
+      .catch(next);
+  }
+);
+
+router.delete("/:id", mw.validateProjectId, (req, res, next) => {
+  Project.remove(req.params.id)
+    .then((project) => {
+      res.json(project);
+    })
+    .catch(next);
+});
+
+router.get("/:id/actions", mw.validateProjectId, (req, res, next) => {
+  Project.getProjectActions(req.params.id)
+    .then((actions) => {
+      res.json(actions);
+    })
+    .catch(next);
+});
+
 // eslint-disable-next-line no-unused-vars
 router.use((err, req, res, next) => {
   res.status(500).json({
